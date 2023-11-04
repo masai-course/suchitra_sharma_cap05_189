7596eb697db6e5670a2a8811a278cd54e0f2d6e8
Sharmasuchi1997 <sharmasuchi1997@gmail.com>
2023-11-04 20:43:20 +0530

    added assignment

---
+++ unit-1/evaluation/avtar_assignment/src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+import Button from "./button";
+
+import Avatar from "./Avatar";
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <Avatar/>
+      <br/>
+      <Button/>
+    </div>
+  );
+}
---
+++ unit-1/evaluation/avtar_assignment/src/Avatar.jsx
@@ -0,0 +1,11 @@
+const Avatar=()=>{
+  return(
+    <>
+    <img className="avatar" src="https://toppng.com/public/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png" alt=""/>
+    <h2>Cool Avatar</h2>
+    
+    </>
+  )
+}
+
+export default Avatar
