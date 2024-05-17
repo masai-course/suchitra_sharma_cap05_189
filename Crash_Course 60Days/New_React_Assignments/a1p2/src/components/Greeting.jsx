05cb3e3eeb70a9122bab3bd2ef2a1456bbfacacf
Sharmasuchi1997 <sharmasuchi1997@gmail.com>
2024-05-18 01:04:54 +0530

    Added new React assignment 1

---
+++ Crash_Course 60Days/New_React_Assignments/a1p2/src/components/Greeting.jsx
@@ -0,0 +1,15 @@
+// src/components/Greeting.jsx
+
+import React from "react";
+
+function Greeting({ message }) {
+
+  
+  return (
+    <div>
+      <h2>{message}</h2> 
+    </div>
+  );
+}
+
+export default Greeting;
---
+++ Crash_Course 60Days/New_React_Assignments/a1p2/src/components/Welcome.jsx
@@ -0,0 +1,14 @@
+// src/components/Welcome.jsx
+
+import React from "react";
+
+function Welcome() {
+  const name = "React Developer";
+  return (
+    <div>
+      <p>Welcome, {name}{/* complete the missing code */}!</p>
+    </div>
+  );
+}
+
+export default Welcome;
